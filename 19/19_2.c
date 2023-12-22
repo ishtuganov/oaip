e532110df9e4dcc22ece6efaeedd896ab43e4e04
ishtuganov <146653031+ishtuganov@users.noreply.github.com>
2023-12-22 07:10:11 +0400

    Add files via upload

---
+++ 19/19_2.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+
+
+int* pa; //Указатель на массив. 
+int n; // Реальное количество элементов в массиве
+
+void Load() {
+	// Открытие входного файла
+	FILE* fin = fopen("in2.txt", "rt");
+	if (fin == NULL) {
+		printf("Входной файл не найден\n");
+		return;
+	}
+	fscanf(fin, "%d", &n);
+	pa = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++) {
+		fscanf(fin, "%d", &pa[i]);
+	}
+	// Закрытие входного файла
+	fclose(fin);
+}
+void SaveResult() {
+	float sa = 0;
+	float s = 0;
+	for (int i = 0; i < n; i++) {
+		s += pa[i];
+	}
+	sa = s / n;
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		if (pa[i] > sa) {
+			m++;
+		}
+	}
+	FILE* fout = fopen("out2.txt", "wt");
+	if (fout == NULL) {
+		printf("Выходной файл не найден\n");
+		return;
+	}
+	fprintf(fout, "%d\n", m);
+	for (int i = 0; i < n; i++) {
+		if (pa[i] > sa) {
+			fprintf(fout, "%d ", pa[i]);
+		}
+	}
+	fclose(fout);
+}
+void main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	printf("Hello! It is Task2!\n");
+
+	Load();
+	SaveResult();
+
+	free(pa);
+}
